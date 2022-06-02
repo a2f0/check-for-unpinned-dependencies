f061da05bcd296e79d9afdcd27e5e1087f67e8e3
Dan Sullivan <dansullivan@gmail.com>
2022-06-01 22:08:30 -0400

    initial commit

---
+++ build/index.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env npx ts-node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+console.info('Checking for pinned dependencies.');
+let packageJson = JSON.parse((0, fs_1.readFileSync)("package.json", "utf8"));
+let dependencies = packageJson.dependencies;
+let devDependencies = packageJson.devDependencies;
+let allDependencies = {
+    ...dependencies,
+    ...devDependencies,
+};
+let unpinnedVersions = {};
+// Semantic versions
+const acceptableVersions = new RegExp('^[0-9]+\\.[0-9]+\\.[0-9]+$');
+for (const entry of Object.entries(allDependencies)) {
+    const dependency = entry[0];
+    const version = entry[1];
+    const isValid = acceptableVersions.test(version);
+    if (isValid === false) {
+        unpinnedVersions[dependency] = version;
+    }
+}
+const unpinnedSize = Object.keys(unpinnedVersions).length;
+if (unpinnedSize > 0) {
+    console.info(`Found ${unpinnedSize} unpinned versions: `);
+    console.info(unpinnedVersions);
+    process.exit(1);
+}
+console.info('No unpinned dependencies found.');
---
+++ index.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env npx ts-node
+import { readFileSync } from 'fs';
+console.info('Checking for pinned dependencies.');
+
+let packageJson = JSON.parse(readFileSync("package.json", "utf8"));
+let dependencies: {[key: string]: string} = packageJson.dependencies;
+let devDependencies: {[key: string]: string} = packageJson.devDependencies;
+
+let allDependencies: {[key: string]: string} = {
+  ...dependencies,
+  ...devDependencies,
+}
+
+let unpinnedVersions: {[key: string]: string} = {}
+// Semantic versions
+const acceptableVersions = new RegExp('^[0-9]+\\.[0-9]+\\.[0-9]+$');
+
+for (const entry of Object.entries(allDependencies)) {
+  const dependency: string = entry[0];
+  const version: string = entry[1];
+  const isValid = acceptableVersions.test(version);
+  if (isValid === false) {
+    unpinnedVersions[dependency] = version;
+  }
+}
+
+const unpinnedSize = Object.keys(unpinnedVersions).length;
+if (unpinnedSize > 0) {
+  console.info(`Found ${unpinnedSize} unpinned versions: `);
+  console.info(unpinnedVersions)
+  process.exit(1);
+}
+console.info('No unpinned dependencies found.')
